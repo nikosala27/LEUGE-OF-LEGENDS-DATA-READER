c7e4cf37609b1a295f8402d0a4a7830476d77710
nikosala27 <167699216+nikosala27@users.noreply.github.com>
2024-06-27 09:54:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from LeaugeDataReader import LeaugeDataReader
 
 reader = LeaugeDataReader()
 nick = ''
-puuid = reader.get_puuid(nick, 'EUNE')
+tag_line = ''
+puuid = reader.get_puuid(nick, tag_line)
 print(puuid)
 reader.get_all_games_info(puuid)
